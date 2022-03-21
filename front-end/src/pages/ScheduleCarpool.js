aa40b6f5aa66e6d534b22a1b2995f94a67320ff9
Bing <bing.q.gu@vanderbilt.edu>
2022-03-21 18:10:24 -0500

    updates to schedule carpool page

--- front-end/src/pages/ScheduleCarpool.js
+++ front-end/src/pages/ScheduleCarpool.js
@@ -27,7 +27,48 @@ function ScheduleCarpool(props) {
           <p class="text">Contact Us</p>
         </div>
       </div> */}
+    
+        <div className="information">
+            <label>First Name:</label>
+            <input
+                type="text"
+            />
+            <label>Last Name:</label>
+            <input
+                type="text"
+            />
+            <label>Date:</label>
+            <input
+                type="date"
+            />
+            <label>Time:</label>
+            <input
+                type="time"
+            />
+        
+            <div class="input-group">
+                <label>Departure Location: </label>
+
+                <select class="custom-select" id="inputGroupSelect">
+                    <option selected>Choose...</option>
+                    <option value="1">Highland</option>
+                    <option value="2">Kissam/EBI</option>
+                    <option value="3">Zeppos</option>
+                    <option value="4">Commons</option>
+                    <option value="5">Blair</option>
+                    <option value="6">Other</option>
+
+                </select>
+                <div class="input-group-append">
+                    <label class="input-group-text" for="inputGroupSelect">Options</label>
+                </div>
+            </div>
+        
+            <button /*onClick={do stuff} */>Add Carpool Request</button>
+        </div>
+
     </div>
+
   );
 }
 
