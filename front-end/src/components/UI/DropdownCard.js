ca42c1a0da22228ed6c5c786c357222239828ecd
Ethan Piper <ethanspam3@gmail.com>
2022-03-21 18:17:44 -0500

    Card Files

---
+++ front-end/src/components/UI/DropdownCard.js
@@ -0,0 +1,33 @@
+import { Card, Row, Col, Button } from "react-bootstrap";
+import classes from "./DropdownCard.module.css";
+
+function DropdownCard(props){
+
+
+	return <div>
+		{props.clicked ? (
+			<Card className={classes.thecard}>
+				<Row>
+					<Col>
+						{props.title}
+						<Button onClick={props.theClick} className={classes.thebutton}>Expand</Button>
+					</Col>
+				</Row>
+			</Card>
+		) : 
+		 <Card className={classes.thecard}>
+			 <Row>
+					<Col>
+						{props.title}
+						<Button onClick={props.theClick} className={classes.thebutton}>Close</Button>
+					</Col>
+				</Row>
+			 <Card.Body>
+				 <Card.Text>{props.text}</Card.Text>
+			 </Card.Body>
+			 </Card>
+			 }
+	</div>
+}
+
+export default DropdownCard;
