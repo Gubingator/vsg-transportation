0a4f7d1858695a4e303b32097d7ec0a2133351ae
Ethan Piper <ethanspam3@gmail.com>
2022-03-21 22:16:40 -0500

    Vanderbilt and Nashville Info

--- front-end/src/pages/Nashville.js
+++ front-end/src/pages/Nashville.js
@@ -13,22 +13,32 @@ import DropdownCard from "../components/UI/DropdownCard";
 import classes from "./Nashville.module.css";
 
 function Nashville(props) {
-  const [VandyVans, setVandyVans] = useState(true);
-  const [MoveVU, setMoveVU] = useState(true);
 
-  function HandleVandyOnClick() {
-    if (VandyVans) {
-      setVandyVans(false);
+  const [WeGO, setWeGO] = useState(true);
+  const [Lyft, setLyft] = useState(true);
+  const [TransitApp, setTransitApp] = useState(true);
+
+  function HandleWeGOnClick() {
+    if (WeGO) {
+      setWeGO(false);
     } else {
-      setVandyVans(true);
+      setWeGO(true);
     }
   }
 
-  function HandleMoveVUOnClick() {
-    if (MoveVU) {
-      setMoveVU(false);
+  function HandleLyftonClick() {
+    if (Lyft) {
+      setLyft(false);
     } else {
-      setMoveVU(true);
+      setLyft(true);
+    }
+  }
+
+  function HandleTransitAppOnClick() {
+    if (TransitApp) {
+      setTransitApp(false);
+    } else {
+      setTransitApp(true);
     }
   }
 
@@ -39,22 +49,33 @@ function Nashville(props) {
         className="img-fluid"
         alt="Responsive image"
       />
-      <h1>Nashville transportation information</h1>
-      <Container className={classes.page}>
+      <h1>Nashville Transportation Information</h1>
+      <Container className={classes.page} fluid>
         <Row className={classes.cardRow}>
           <DropdownCard
-            clicked={VandyVans}
-            title="VandyVans"
-            theClick={HandleVandyOnClick}
-            text={"This is a test"}
+            clicked={WeGO}
+            title="WeGO"
+            theClick={HandleWeGOnClick}
+            text={"WeGo Public Transit is the website for the bus sytems around Nashville."}
+            link={"https://www.wegotransit.com/"}
           ></DropdownCard>
         </Row>
         <Row className={classes.cardRow}>
           <DropdownCard
-            clicked={MoveVU}
-            title="MoveVU"
-            theClick={HandleMoveVUOnClick}
+            clicked={Lyft}
+            title="Lyft"
+            theClick={HandleLyftonClick}
             text={"This is a test 2"}
+            link = {""}
+          ></DropdownCard>
+        </Row>
+        <Row className={classes.cardRow}>
+          <DropdownCard
+            clicked={TransitApp}
+            title="TransitApp"
+            theClick={HandleTransitAppOnClick}
+            text={"This is a test 3"}
+            link ={"https://transitapp.com/region/nashville"}
           ></DropdownCard>
         </Row>
       </Container>
--- front-end/src/pages/Vanderbilt.js
+++ front-end/src/pages/Vanderbilt.js
@@ -1,12 +1,65 @@
+import { useState } from "react";
+import {
+  Card,
+  Row,
+  Col,
+  Button,
+  CloseButton,
+  Dropdown,
+  DropdownButton,
+  Container,
+} from "react-bootstrap";
+import DropdownCard from "../components/UI/DropdownCard";
+import classes from "./Vanderbilt.module.css";
+
 function Vanderbilt(props) {
+  const [VandyVans, setVandyVans] = useState(true);
+  const [MoveVU, setMoveVU] = useState(true);
+
+  function HandleVandyOnClick() {
+    if (VandyVans) {
+      setVandyVans(false);
+    } else {
+      setVandyVans(true);
+    }
+  }
+
+  function HandleMoveVUOnClick() {
+    if (MoveVU) {
+      setMoveVU(false);
+    } else {
+      setMoveVU(true);
+    }
+  }
+
   return (
     <div>
       <img
         src="https://res.cloudinary.com/hud9ala09/image/upload/v1457044154/zkv3yncyffd3p7aucyb8.png"
-        class="img-fluid"
+        className="img-fluid"
         alt="Responsive image"
       />
-      <h1> Vanderbilt transportation information</h1>
+      <h1> Vanderbilt Transportation Information</h1>
+      <Container className={classes.page} fluid>
+        <Row className={classes.cardRow}>
+          <DropdownCard
+            clicked={VandyVans}
+            title="VandyVans"
+            theClick={HandleVandyOnClick}
+            text={"This is a test"}
+            link = {""}
+          ></DropdownCard>
+        </Row>
+        <Row className={classes.cardRow}>
+          <DropdownCard
+            clicked={MoveVU}
+            title="MoveVU"
+            theClick={HandleMoveVUOnClick}
+            text={"This is a test 2"}
+            link={""}
+          ></DropdownCard>
+        </Row>
+      </Container>
     </div>
   );
 }
