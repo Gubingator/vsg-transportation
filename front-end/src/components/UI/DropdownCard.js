cfd7832765140356387424b6370776fc7df6b01f
Ethan Piper <ethanspam3@gmail.com>
2022-03-21 20:36:16 -0500

    Transit Links

--- front-end/src/components/UI/DropdownCard.js
+++ front-end/src/components/UI/DropdownCard.js
@@ -1,33 +1,44 @@
 import { Card, Row, Col, Button } from "react-bootstrap";
 import classes from "./DropdownCard.module.css";
 
-function DropdownCard(props){
+function DropdownCard(props) {
+	console.log(props.link);
 
-
-	return <div>
-		{props.clicked ? (
-			<Card className={classes.thecard}>
-				<Row>
-					<Col>
-						{props.title}
-						<Button onClick={props.theClick} className={classes.thebutton}>Expand</Button>
-					</Col>
-				</Row>
-			</Card>
-		) : 
-		 <Card className={classes.thecard}>
-			 <Row>
-					<Col>
-						{props.title}
-						<Button onClick={props.theClick} className={classes.thebutton}>Close</Button>
-					</Col>
-				</Row>
-			 <Card.Body>
-				 <Card.Text>{props.text}</Card.Text>
-			 </Card.Body>
-			 </Card>
-			 }
-	</div>
+  return (
+    <div>
+      {props.clicked ? (
+        <Card className={classes.thecard}>
+          <Row>
+            <Col>
+              {props.title}
+              <Button onClick={props.theClick} className={classes.thebutton}>
+                Expand
+              </Button>
+            </Col>
+          </Row>
+        </Card>
+      ) : (
+        <Card className={classes.thecard}>
+          <Row>
+            <Col>
+              {props.title}
+              <Button
+                onClick={props.theClick}
+                className={classes.thebutton}
+                active
+              >
+                Close
+              </Button>
+            </Col>
+          </Row>
+          <Card.Body>
+            <Card.Text>{props.text}</Card.Text>
+						<a href={props.link}>Link to {props.title}</a>
+          </Card.Body>
+        </Card>
+      )}
+    </div>
+  );
 }
 
 export default DropdownCard;
