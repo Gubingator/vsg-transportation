400925417fb34df950896902adef8d2bb5463413
Sarah Zhang <sarah.s.zhang@vanderbilt.edu>
2022-04-21 11:51:43 -0500

    updated setup_db.sql

--- back-end/setup_db.sql
+++ back-end/setup_db.sql
@@ -8,7 +8,7 @@ CREATE TABLE carpools (
     departure VARCHAR(255),
     destination VARCHAR(255),
     date_time DATETIME,
-    filled_seats INT,
+    filled_seats INTEGER DEFAULT 0,
     groupme_link VARCHAR(255) DEFAULT NULL
 );
 
@@ -18,11 +18,11 @@ DROP TABLE IF EXISTS passengers;
 CREATE TABLE passengers (
     passenger_id INTEGER PRIMARY KEY AUTO_INCREMENT, 
     carpool_id INTEGER, 
-    name VARCHAR(255), 
+    name VARCHAR(255),
     email VARCHAR(255), 
     code VARCHAR(255), 
-    created_at DATETIME, 
-    verified INT DEFAULT FALSE,
+    created_at DATETIME DEFAULT NOW(), 
+    verified INTEGER DEFAULT FALSE,
     CONSTRAINT carpool_fk 
         FOREIGN KEY (carpool_id) 
         REFERENCES carpool (id) 
